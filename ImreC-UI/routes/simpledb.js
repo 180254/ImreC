c5c09a8893d5e46a805f94d22ca4d7dd4a3656e0
Adrian Pędziwiatr <180254@edu.p.lodz.pl>
2016-05-02 09:27:39 +0200

    (ui-app) SimpleDb debug content

---
+++ ImreC-UI/routes/simpledb.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var express = require('express');
+var router = express.Router();
+
+var utils = require('../helpers/utils');
+var conf = utils.readJson('conf.json');
+
+var AWS = require('aws-sdk');
+AWS.config.loadFromPath('./config.json');
+
+var simpledb = new AWS.SimpleDB();
+
+router.get('/', function (req, res, next) {
+    res.setHeader('Content-Type', 'application/json');
+
+    var params = {
+        SelectExpression: 'select * from ' + conf.SimpleDb.Domain
+    };
+
+    simpledb.select(params, function (err, data) {
+        if (err) res.send(JSON.stringify(err.stack, null, ' '));
+        else     res.send(JSON.stringify(data, null, ' '));
+    });
+});
+
+module.exports = router;
