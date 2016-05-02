ac41fb1a26b56100eeb440eb8ac00067af063ea4
Adrian Pędziwiatr <180254@edu.p.lodz.pl>
2016-05-02 20:47:31 +0200

    (cc-app) displaying processing status implemented; results may be downloaded

---
+++ ImreC-Cc/helpers/status.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var utils = require('../helpers/utils');
+var conf = utils.readJson('conf.json');
+
+var AWS = require('aws-sdk');
+AWS.config.loadFromPath('./config.json');
+var s3 = new AWS.S3();
+
+var statusDescriptions = [
+    'File successfully uploaded. Resize task to be scheduled. Please wait.',
+    'File successfully uploaded. Resize task scheduled. Please wait.',
+    'File processed. Results ready to download.'
+];
+
+var getStatus = function (bucketKey, callbackOk, callbackFail) {
+    var headParams = {
+        Bucket: conf.S3.Name,
+        Key: bucketKey
+    };
+
+    s3.headObject(headParams, function (err, data) {
+        if (err) {
+            callbackFail();
+
+        } else {
+            var renderParams = {
+                id: bucketKey,
+                url: conf.S3.Url + '/' + bucketKey,
+                status: statusDescriptions[Number.parseInt(data.Metadata.workstatus)]
+            };
+
+            callbackOk(renderParams);
+        }
+    });
+};
+
+exports.getStatus = getStatus;
+
--- ImreC-Cc/routes/task.js
+++ ImreC-Cc/routes/task.js
@@ -5,6 +5,8 @@ var router = express.Router();
 
 var utils = require('../helpers/utils');
 var scheduler = require('../helpers/scheduler');
+var status = require('../helpers/status');
+
 
 router.get('/schedule', function (req, res, next) {
     var scheduleParams = {
@@ -15,6 +17,16 @@ router.get('/schedule', function (req, res, next) {
 
     res.render('task/schedule', utils.clone(scheduleParams));
     scheduler.scheduleTaskIfNew(req, scheduleParams);
+});
+
+
+router.get('/', function (req, res, next) {
+
+    status.getStatus(req.query.id, function (renderParams) {
+        res.render('task/status', renderParams); // ok
+    }, function () {
+        res.sendStatus(404); // fail
+    });
 });
 
 module.exports = router;
