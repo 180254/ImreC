bba77f8f0e3879f8c74c01a0257a9adfa1364822
Adrian Pędziwiatr <180254@edu.p.lodz.pl>
2016-05-02 09:27:02 +0200

    (ui-app) init SimpleDb on start

---
+++ ImreC-UI/onstart.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var AWS = require('aws-sdk');
+AWS.config.loadFromPath('./config.json');
+var simpleDb = new AWS.SimpleDB();
+
+var utils = require('./helpers/utils');
+var conf = utils.readJson('conf.json');
+
+var actionRemaining = 1;
+var callbackFunc = null;
+
+var checkStartDone = function () {
+    if (--actionRemaining <= 0) {
+        callbackFunc();
+    }
+};
+
+var onStart = function (callback) {
+    callbackFunc = callback;
+
+    // simpleDbInit();
+    checkStartDone();
+};
+
+var simpleDbInit = function () {
+    var simpleDbParams = { DomainName: conf.SimpleDb.Domain };
+
+    simpleDb.deleteDomain(simpleDbParams, function (err, data) {
+        if (err) throw new Error(err.stack);
+        else {
+            simpleDb.createDomain(simpleDbParams, function (err, data) {
+                if (err)  throw new Error(err.stack);
+                else      checkStartDone();
+            });
+        }
+    });
+};
+
+exports.onStart = onStart;
