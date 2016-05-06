f5368a1ef8fc224b63b2d4c8ea573acf432d67c7
Adrian Pędziwiatr <180254@edu.p.lodz.pl>
2016-05-06 22:40:44 +0200

    (wr-app)  sdb logic improvement

--- ImreC-Wr/src/main/java/p/lodz/pl/adi/utils/Logger.java
+++ ImreC-Wr/src/main/java/p/lodz/pl/adi/utils/Logger.java
@@ -1,9 +1,12 @@
 package p.lodz.pl.adi.utils;
 
 import com.amazonaws.AmazonClientException;
+import org.apache.commons.lang3.tuple.Pair;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Collection;
 
 public class Logger {
 
@@ -19,6 +22,7 @@ public class Logger {
             logInternal(true, action, args);
 
         } catch (AmazonClientException ignored) {
+            System.out.println("? - Unable to send log.");
         }
     }
 
@@ -28,13 +32,30 @@ public class Logger {
 
     private void logInternal(boolean sdbPut, String action, Object... args) {
         String text = String.format("1 | %s | ? | %s", LocalDateTime.now().format(dtf), action);
-
         for (Object mes : args) {
             String mesAsString = mes != null ? mes.toString() : "NULL";
             text += String.format(" | %s", mesAsString);
         }
 
         System.out.println(text);
-        if (sdbPut) am.sdb$putLogAsync(text);
+
+
+        if (sdbPut) {
+            Collection<Pair<String, String>> attrs = new ArrayList<>();
+            attrs.add(Pair.of("0_Source", "1"));
+            attrs.add(Pair.of("1_Date", LocalDateTime.now().format(dtf)));
+            attrs.add(Pair.of("2_IP", "?"));
+            attrs.add(Pair.of("3_Action", action));
+
+
+            for (int i = 0; i < args.length; i++) {
+                Object mes = args[i];
+                String mesAsString = mes != null ? mes.toString() : "NULL";
+
+                attrs.add(Pair.of("4_Arg_" + i, mesAsString));
+            }
+
+            am.sdb$putLogAsync(attrs);
+        }
     }
 }
