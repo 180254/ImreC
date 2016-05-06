fb6a1dd4ddfdb19c6ab21a153bea5d5edb0d2191
Adrian Pędziwiatr <180254@edu.p.lodz.pl>
2016-05-07 00:25:36 +0200

    (cc-app) working way of collecting ip

--- ImreC-Cc/helpers/selfip.js
+++ ImreC-Cc/helpers/selfip.js
@@ -1,20 +1,11 @@
 'use strict';
 
-var publicIp = require('public-ip');
+var eIP = require('external-ip')();
 var myPublicIp = '?';
 
-publicIp.v4(function (err, ip) {
-    console.log('v4');
-    console.log(err);
-    console.log(ip);
-    if (ip) myPublicIp = ip;
-});
-
-publicIp.v6(function (err, ip) {
-    console.log('v6');
-    console.log(err);
-    console.log(ip);
-    if (ip) myPublicIp = ip;
+eIP(function (err, ip) {
+    if (!err)
+        myPublicIp = ip;
 });
 
 var ip = function () {
